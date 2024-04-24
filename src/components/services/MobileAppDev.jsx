a906fbf0ef48ab1d54b04dbb1ea35a240cf7b1a7
Yaman <yamanmahawar10@gmail.com>
2024-04-24 12:37:46 +0530

    added all files

---
+++ src/components/services/MobileAppDev.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import "../../assets/Component.css";
+import {
+  FaTwitter,
+  FaFacebook,
+  FaInstagram,
+  FaYoutube,
+  FaLinkedin,
+} from "react-icons/fa";
+
+const development = [
+  {
+    icon: <FaTwitter />,
+    href: "https://www.linkedin.com/in/yaman-mahawar-a29040231/",
+  },
+  {
+    icon: <FaFacebook />,
+    href: "https://www.linkedin.com/in/yaman-mahawar-a29040231/",
+  },
+  {
+    icon: <FaInstagram />,
+    href: "https://www.linkedin.com/in/yaman-mahawar-a29040231/",
+  },
+  {
+    icon: <FaYoutube />,
+    href: "https://www.linkedin.com/in/yaman-mahawar-a29040231/",
+  },
+  {
+    icon: <FaLinkedin />,
+    href: "https://www.linkedin.com/in/yaman-mahawar-a29040231/",
+  },
+];
+const MobileAppDev = () => {
+  return (
+    <div className="border border-black w-full  bg-customPurple">
+      <div className="mx-auto max-w-[1920px] px-4 sm:px-10 ">yanma</div>
+    </div>
+  );
+};
+
+export default MobileAppDev;
---
+++ src/pages/Services.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import MobileAppDev from '../components/services/MobileAppDev'
+import WebSolution from '../components/services/WebSolution'
+import Ecommerce from '../components/services/Ecommerce'
+import Artificial from '../components/services/Artificial'
+import Design from '../components/services/Design'
+import Devops from '../components/services/Devops'
+
+const Services = () => {
+    return (
+        <div>
+            <MobileAppDev/>
+            <WebSolution/>
+            <Ecommerce/>
+            <Artificial/>
+            <Design/>
+            <Devops/>
+        </div>
+    )
+}
+
+export default Services
